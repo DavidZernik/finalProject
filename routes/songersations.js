1d005b36ecb27cf59994d8b62bf0bc6ce4c0227f
David Zernik <dzernik@gmail.com>
2015-09-24 17:05:14 -0700

    THURSDAY

--- routes/songersations.js
+++ routes/songersations.js
@@ -4,17 +4,17 @@ var bodyParser = require('body-parser'); //parses information from POST
 var Songersation = require('../models/songersation');
 
 //show all the songersations that exist
-songersationsRouter.get('/',function(request,response){
-  Songersation.find({}, function(err,songersations){
-    if(err) return console.log(err);
-    response.json(songersations);
-  });
-});
-//Post a songersation
-songersationsRouter.post('/', function (request,response) {
-  Songersation.create(request.body, function (err, newSongersation) {
-    if(err) return console.log(err);
-    response.json(newSongersation);
-  });
-});
+// songersationsRouter.get('/',function(request,response){
+//   Songersation.find({}, function(err,songersations){
+//     if(err) return console.log(err);
+//     response.json(songersations);
+//   });
+// });
+// //Post a songersation
+// songersationsRouter.post('/', function (request,response) {
+//   Songersation.create(request.body, function (err, newSongersation) {
+//     if(err) return console.log(err);
+//     response.json(newSongersation);
+//   });
+// });
 module.exports = songersationsRouter;
